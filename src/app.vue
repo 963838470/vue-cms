49639388b3099f827060c6a032dc0171337593de
r720 <963838470@qq.com>
2017-12-13 22:02:08 +0800

    头尾

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>终于等到你，还好我没放弃</div>
+  <div>
+    <mt-header title="信息管理系统"></mt-header>
+    <router-view></router-view>
+    		<nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+  </div>  
 </template>
 
 <script>
 export default {
   data() {
     return {};
+  },
+  created() {
+    // this.$ajax.get("https://www.baidu.com").then(res => {
+    //   console.log(res);
+    // });
   }
 };
 </script>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 'use strict';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import Axios from 'axios';
+import './static/vendor/mui/dist/css/mui.css'
+
 import App from './app.vue';
+import Home from './components/home.vue';
+
+Vue.use(VueRouter);
+Vue.use(Mint);
+Vue.prototype.$ajax = Axios;
+
+let router = new VueRouter({
+    routes: [
+        { path: '/', redirect: { name: 'home' } },
+        { name: 'home', path: '/home', component: Home },
+
+
+    ]
+})
 
 new Vue({
     el: "#app",
+    router,
     render: o => o(App)
 })
