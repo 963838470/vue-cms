41daa716a6f0e222987019184a7996484300e2e4
chenbo <963838470@qq.com>
2017-12-18 12:28:22 +0800

    上拉加载更多

--- src/components/goods/list.vue
+++ src/components/goods/list.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="tmpl">
-    <load></load>
-    <!-- <nav-bar title="商品列表"></nav-bar>
-    <mt-loadmore :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore">
-      <ul>
-        <li v-for="(good,index) in goods" :key="index">
-          <div>
-            <img :src="good.src">
-            <span>{{ good.title }}</span>
+    <nav-bar title="商品列表"></nav-bar>
+    <div class="page-loadmore-wrapper" ref="wrapper">
+      <mt-loadmore :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore">
+        <ul>
+          <li v-for="(good,index) in goods" :key="index">
             <div>
-              <span>￥{{ good.price }}</span>
-              <span>
-                <del>￥{{ good.marketPrice }}</del>
-              </span>
+              <img :src="good.src">
+              <span>{{ good.title }}</span>
+              <div>
+                <span>￥{{ good.price }}</span>
+                <span>
+                  <del>￥{{ good.marketPrice }}</del>
+                </span>
+              </div>
             </div>
-          </div>
-        </li>
-      </ul>
-    </mt-loadmore> -->
+          </li>
+        </ul>
+      </mt-loadmore>
+    </div>
   </div>
 </template>
 
 <script>
-import Load from './loadmore.vue'
 export default {
   data: function() {
     return {
@@ -32,66 +32,65 @@ export default {
   },
   methods: {
     loadBottom: function() {
-      console.log("触发loadmore函数");
-      this.goods = this.goods.concat(
-        {
-          title: "日落",
-          src: require("../../img/1.jpg"),
-          price: 2100,
-          marketPrice: 3000
-        },
-        {
-          title: "海豹",
-          src: require("../../img/2.jpg"),
-          price: 18888,
-          marketPrice: 20000
-        },
-        {
-          title: "蜗牛",
-          src: require("../../img/3.jpg"),
-          price: 18,
-          marketPrice: 36
-        },
-        {
-          title: "日落",
-          src: require("../../img/1.jpg"),
-          price: 2100,
-          marketPrice: 3000
-        },
-        {
-          title: "海豹",
-          src: require("../../img/2.jpg"),
-          price: 18888,
-          marketPrice: 20000
-        },
-        {
-          title: "蜗牛",
-          src: require("../../img/3.jpg"),
-          price: 18,
-          marketPrice: 36
-        },
-        {
-          title: "日落",
-          src: require("../../img/1.jpg"),
-          price: 2100,
-          marketPrice: 3000
-        },
-        {
-          title: "海豹",
-          src: require("../../img/2.jpg"),
-          price: 18888,
-          marketPrice: 20000
-        }
-      );
-      //this.allLoaded = false;
-      //this.$refs.loadmore.onBottomLoaded();
+      setTimeout(() => {
+        console.log("触发loadmore函数");
+        this.goods = this.goods.concat(
+          {
+            title: "日落",
+            src: require("../../img/1.jpg"),
+            price: 2100,
+            marketPrice: 3000
+          },
+          {
+            title: "海豹",
+            src: require("../../img/2.jpg"),
+            price: 18888,
+            marketPrice: 20000
+          },
+          {
+            title: "蜗牛",
+            src: require("../../img/3.jpg"),
+            price: 18,
+            marketPrice: 36
+          },
+          {
+            title: "日落",
+            src: require("../../img/1.jpg"),
+            price: 2100,
+            marketPrice: 3000
+          },
+          {
+            title: "海豹",
+            src: require("../../img/2.jpg"),
+            price: 18888,
+            marketPrice: 20000
+          },
+          {
+            title: "蜗牛",
+            src: require("../../img/3.jpg"),
+            price: 18,
+            marketPrice: 36
+          },
+          {
+            title: "日落",
+            src: require("../../img/1.jpg"),
+            price: 2100,
+            marketPrice: 3000
+          },
+          {
+            title: "海豹",
+            src: require("../../img/2.jpg"),
+            price: 18888,
+            marketPrice: 20000
+          }
+        );
+        //this.allLoaded = false;
+        this.$refs.loadmore.onBottomLoaded();
+      }, 1500);
     }
   },
   created() {
     this.loadBottom();
-  },
-  components: {
-    Load
   }
 };
 </script>
@@ -122,5 +121,9 @@ li > div > div > span:nth-child(1) {
 
 li > div > div > span:nth-child(2) {
   float: right;
+}
+
+.page-loadmore-wrapper {
+  overflow: scroll;
 }
 </style>
