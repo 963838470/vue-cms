70f569bc52bac5923dd5668fb97d357ae4c2e8b5
unknown <chenbo@huawei>
2017-12-29 17:42:20 +0800

    配置文件调整

--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
 'use strict';
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Moment from 'moment'
+import App from './app.vue';
+
+// ui
 import Mint from 'mint-ui';
 import 'mint-ui/lib/style.css';
-import Axios from 'axios';
 import './static/vendor/mui/dist/css/mui.css'
-import Moment from 'moment'
-import VuePreview from 'vue-preview'
-
-import App from './app.vue';
-import Home from './components/home/home.vue';
-const Member = resolve => require(["./components/member/member.vue"], resolve)
-//import Member from './components/member/member.vue';
-import Search from './components/search/search.vue';
-import Shopcart from './components/shopcart/shopcart.vue';
-import NewsList from './components/news/list.vue';
-import NewsDetail from './components/news/detail.vue';
-import NavBar from './components/common/navBar.vue';
-import Comment from './components/common/comment.vue';
-import PhotoShare from './components/photo/share.vue';
-import PhotoDetail from './components/photo/detail.vue';
-import GoodsList from './components/goods/list.vue';
-import GoodsDetail from './components/goods/detail.vue';
-import GoodsComment from './components/goods/comment.vue';
 require('./static/css/global.css')
-
-Vue.use(VueRouter);
 Vue.use(Mint);
-Vue.use(VuePreview)
-Vue.prototype.$ajax = Axios;
 
+// 图片预览插件
+import VuePreview from 'vue-preview'
+Vue.use(VuePreview)
+
+// ajax插件
+import Axios from 'axios';
+Vue.prototype.$ajax = Axios;
 Axios.defaults.baseURL = 'http://182.254.146.100:8899/api';
 Axios.interceptors.request.use(function (config) {
     Mint.Indicator.open({
@@ -44,20 +31,29 @@ Axios.interceptors.response.use(function (config) {
     return config;
 })
 
+// 自定义组件
+const NavBar = resolve => require(['./components/common/navBar.vue'], resolve)
+Vue.component("nav-bar", NavBar)
+const Comment = resolve => require(['./components/common/comment.vue'], resolve)
+Vue.component("comment", Comment)
+
+// 路由配置
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 let router = new VueRouter({
     routes: [
         { path: '/', redirect: { name: 'home' } },
-        { name: 'home', path: '/home', component: Home },
-        { name: 'member', path: '/member', component: Member },
-        { name: 'search', path: '/search', component: Search },
-        { name: 'shopcart', path: '/shopcart', component: Shopcart },
-        { name: 'news.list', path: '/news/list', component: NewsList },
-        { name: 'news.detail', path: '/news/detail/:id', component: NewsDetail },
-        { name: 'photo.share', path: '/photo/share', component: PhotoShare },
-        { name: 'photo.detail', path: '/photo/detail/:id', component: PhotoDetail },
-        { name: 'goods.list', path: '/photo/goods/list', component: GoodsList },
-        { name: 'goods.detail', path: '/photo/goods/detail', component: GoodsDetail },
-        { name: 'goods.comment', path: '/photo/goods/comment', component: GoodsComment },
+        { name: 'home', path: '/home', component: resolve => require(["./components/home/home.vue"], resolve) },
+        { name: 'member', path: '/member', component: resolve => require(["./components/member/member.vue"], resolve) },
+        { name: 'search', path: '/search', component: resolve => require(['./components/search/search.vue'], resolve) },
+        { name: 'shopcart', path: '/shopcart', component: resolve => require(['./components/shopcart/shopcart.vue'], resolve) },
+        { name: 'news.list', path: '/news/list', component: resolve => require(['./components/news/list.vue'], resolve) },
+        { name: 'news.detail', path: '/news/detail/:id', component: resolve => require(['./components/news/detail.vue'], resolve) },
+        { name: 'photo.share', path: '/photo/share', component: resolve => require(['./components/photo/share.vue'], resolve) },
+        { name: 'photo.detail', path: '/photo/detail/:id', component: resolve => require(['./components/photo/detail.vue'], resolve) },
+        { name: 'goods.list', path: '/photo/goods/list', component: resolve => require(['./components/goods/list.vue'], resolve) },
+        { name: 'goods.detail', path: '/photo/goods/detail', component: resolve => require(['./components/goods/detail.vue'], resolve) },
+        { name: 'goods.comment', path: '/photo/goods/comment', component: resolve => require(['./components/goods/comment.vue'], resolve) },
 
     ],
     linkActiveClass: "mui-active",
@@ -70,14 +66,12 @@ let router = new VueRouter({
     }
 })
 
+// 过滤器
 Vue.filter("filterDate", function (value) {
     return Moment(value).format('YYYY-MM-DD');
 })
 
-Vue.component("nav-bar", NavBar)
-Vue.component("comment", Comment)
-
-
+// 启动入口
 new Vue({
     el: "#app",
     router,
